7984ecfc915b985903920daec462b813c8ca4e79
ceciliawambui <ceciliawambui026@gmail.com>
2022-01-17 16:43:43 +0300

    Performed Arithmetic operations

---
+++ operator.php
@@ -0,0 +1,13 @@
+<!-- Arithmetic operations are carried out;addition, subtraction, division etc -->
+<?php
+
+$a=10;
+$b=20;
+$sum = $a + $b;
+$division = $b / $a;
+$multiply = $a * $b;
+echo $sum;
+echo $division;
+echo $multiply;
+
+?>
