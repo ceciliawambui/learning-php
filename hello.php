ffa7e17c3f4a7416eb01a50d702b0b5d3a231a9e
ceciliawambui <ceciliawambui026@gmail.com>
2022-01-17 16:22:39 +0300

    First program in PHP

---
+++ hello.php
@@ -0,0 +1,8 @@
+<!-- Successfully installed php..yaay!! -->
+<!-- My first program in php...Good job! -->
+<!-- To many more code...yaay!! -->
+<?php
+echo "Hello, world!";
+echo "First program in PHP!";
+echo "Yaaaay!!!";
+?>
