c65a9f1ed506c65be54673b8cce3dcdffe3b9e0a
ceciliawambui <ceciliawambui026@gmail.com>
2022-01-17 16:47:35 +0300

    Added description to echo

--- operator.php
+++ operator.php
@@ -6,8 +6,9 @@ $b=20;
 $sum = $a + $b;
 $division = $b / $a;
 $multiply = $a * $b;
-echo $sum;
-echo $division;
-echo $multiply;
+
+echo "The sum is".$sum;
+echo "The division is" .$division;
+echo "The multiplication is".$multiply;
 
 ?>
