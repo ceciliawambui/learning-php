43a5a3af3fd14356b3635b4034d9bf933d14629a
ceciliawambui <ceciliawambui026@gmail.com>
2022-01-18 09:02:27 +0300

    String formatting using defined variables

--- html-test.php
+++ html-test.php
@@ -8,11 +8,16 @@
 </head>
 <body>
     <?php
-    echo "<h1> Wambui's Site</h1>";
+    echo "<h1>Wambui's Site</h1>";
  
     echo "<h2>Hello, World!</h2>";
     echo "<h3>Welcome to PHP World!</h3>";
-    echo "<hr>"
+    echo "<h4>There once lived a 50 year old man.</h4>";
+    echo "<h4>He was married and had two daughters.</h4>";
+    echo "<h4><em>They lived in California.</em></h4>";
+    echo "<h4>They were filthy rich.</h4>";
+    echo "<h4>They had a 200 hectares ranch.</h4>";
+    echo "<hr>";
     ?>
     
 </body>
---
+++ string-formatting.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- double quoted strings can format strings using defined variables -->
+<?php
+$name="Wesley";
+$school="Othaya";
+echo "<h2>His name is</h2> $name";
+echo "<h2>He schooled at </h2> $school";
+
+?>
+    
+</body>
+</html>
+
+
+
