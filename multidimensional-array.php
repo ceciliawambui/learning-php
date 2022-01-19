9a48ea406d0a2138ede691e85aac63d223de3580
ceciliawambui <ceciliawambui026@gmail.com>
2022-01-19 12:37:19 +0300

    Multidimensional array

---
+++ multidimensional-array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // contains two or more arrays
+    $multiarray =[[1,2,3], [4,5,6], [7,8,9],];
+
+    //the array in the third position, index 2, in the multi array
+    print_r($multiarray[2]);
+
+    //to get what value in one array in the multiple arrays
+    print_r($multiarray[2][0]);
+
+    ?>
+
+    
+</body>
+</html>
