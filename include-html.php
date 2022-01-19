2db075c4984a42ff79d686ce3803d4c453c9e54d
ceciliawambui <ceciliawambui026@gmail.com>
2022-01-19 09:06:10 +0300

    Including html on php file

---
+++ include-html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include "header.html" ?>
+    <p>Welcome to my little World</p>
+    <?php include "footer.html" ?>
+    
+</body>
+</html>
